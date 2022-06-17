782ab117319f26626f788180201024a2e33cf8c1
Merybar <regenboogtranen22@gmail.com>
2022-06-17 13:10:05 +0200

    db + graph

--- resources/views/vendor/voyager/animals/read.blade.php
+++ resources/views/vendor/voyager/animals/read.blade.php
@@ -116,10 +116,31 @@
                                                 const myChart = new Chart(ctx, {
                                                     type: 'line',
                                                     data: {
-                                                        labels: ['Log1', 'Log2', 'Log3', 'Log4', 'Log5', 'Log6'],
+                                                        labels: ['12Nov', '15Nov', '23Nov', '4Dec', '16Dec', '20Dec'],
                                                         datasets: [{
                                                             label: 'Weight',
                                                             data: [12, 19, 3, 5, 2, 3],
+                                                            backgroundColor: [
+                                                                'rgba(255, 99, 132, 0.2)',
+                                                                'rgba(54, 162, 235, 0.2)',
+                                                                'rgba(255, 206, 86, 0.2)',
+                                                                'rgba(75, 192, 192, 0.2)',
+                                                                'rgba(153, 102, 255, 0.2)',
+                                                                'rgba(255, 159, 64, 0.2)'
+                                                            ],
+                                                            borderColor: [
+                                                                'rgba(255, 99, 132, 1)',
+                                                                'rgba(54, 162, 235, 1)',
+                                                                'rgba(255, 206, 86, 1)',
+                                                                'rgba(75, 192, 192, 1)',
+                                                                'rgba(153, 102, 255, 1)',
+                                                                'rgba(255, 159, 64, 1)'
+                                                            ],
+                                                            borderWidth: 1
+                                                        },
+                                                        {
+                                                            label: 'Size',
+                                                            data: [13, 18, 14, 20, 21, 20],
                                                             backgroundColor: [
                                                                 'rgba(255, 99, 132, 0.2)',
                                                                 'rgba(54, 162, 235, 0.2)',
