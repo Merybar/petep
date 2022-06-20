7ca96a189b639290377f525c6c63a543c1d59c34
mdenitti <massimodn77@gmail.com>
2022-06-20 15:12:55 +0200

    Custom footer

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+      
+            <b><i class="voyager-rum"></i> Made by Lotte & Yakup</b> - SyntraPXL FullStack 
+      
+       
+    </div>
+</footer>
