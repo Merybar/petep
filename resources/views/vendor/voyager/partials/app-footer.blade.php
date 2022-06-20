f23f586c364038a18ff46483c94c5685f5055289
mdenitti <massimodn77@gmail.com>
2022-06-20 15:19:52 +0200

    Revert "Custom footer"
    This reverts commit 7ca96a189b639290377f525c6c63a543c1d59c34.

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="app-footer">
-    <div class="site-footer-right">
-      
-            <b><i class="voyager-rum"></i> Made by Lotte & Yakup</b> - SyntraPXL FullStack 
-      
-       
-    </div>
-</footer>
